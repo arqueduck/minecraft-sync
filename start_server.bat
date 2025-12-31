d2c835f50a6a300b76ff79f4f0db31aa230cdce5
Gustavo-Aal <gustavomarquesaal@gmail.com>
2025-12-31 13:31:30 -0300

    save: mundo atualizado por DESKTOP-1T5CLNA

--- start_server.bat
+++ start_server.bat
@@ -27,11 +27,20 @@ git add server.lock
 git commit -m "lock: %COMPUTERNAME% iniciou o servidor"
 git push
 
+
 REM --- inicia servidor ---
-echo ▶️ Iniciando servidor...
-call run.bat
 
 set WEBHOOK_URL=https://discord.com/api/webhooks/1455941349835542783/coQxn5rWwNDQ4Qz1iD0s3_dYld2Fajg6lIvAG1bBwHgs0i3ToQL989KGE3KcgM3L5HR6
+
+set MSG=▶️ Iniciando servidor...
+echo %MSG%
+
+curl -H "Content-Type: application/json" ^
+  -d "{\"content\":\"%MSG%\"}" ^
+  "%WEBHOOK_URL%"
+
+start "Minecraft Server" cmd /k call run.bat
+
 set MSG=🟢 O servidor está rodando
 
 curl -H "Content-Type: application/json" ^
